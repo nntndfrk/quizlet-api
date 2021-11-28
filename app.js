34666e6b47b34f70f0a5a35ae3a9142ebb51132f
nntndfrk <nntndfrk@gmail.com>
2021-11-28 18:54:48 +0200

    added cors support 2

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const dotenv = require("dotenv");
 
 try {
